b879e5dfea7237682992c34bf79d4f661ca99b80
Assmannvfbh <niklasdautphetal@gmail.com>
2022-06-12 18:31:58 +0200

    Model erste Ideen

---
+++ app/src/main/java/Model/MyCourses.java
@@ -0,0 +1,8 @@
+package Model;
+
+import java.util.List;
+
+public class MyCourses {
+
+    private List<Course> myCourseList;
+}
---
+++ app/src/main/java/Model/Room.java
@@ -0,0 +1,11 @@
+package Model;
+
+import android.location.Location;
+
+public abstract class Room {
+    protected String roomNumber;
+    protected double longitudeGEO;
+    protected double latitudeGEO;
+    protected String building;
+    protected List<>
+}
